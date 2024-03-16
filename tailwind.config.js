691041bb4982d3cbef913b9331d461f1cf47e6af
zhaleh <zhalehelmi@gmail.com>
2024-03-16 21:11:53 +0330

    header

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brown: {
+          100: "#ECE0D1",
+          300: "#DBC1AC",
+          600: "#967259",
+          900: "#634832",
+        },
+      },
+      boxShadow: {
+        normal: "0px 1px 10px rgba(0,0,0,.05)",
+      },
+      borderRadius: {
+        "4xl": "2rem",
+      },
+      fontFamily: {
+        Yekan: "Yekan",
+        Lalezar: "Lalezar",
+      },
+    },
   },
   plugins: [],
 };
